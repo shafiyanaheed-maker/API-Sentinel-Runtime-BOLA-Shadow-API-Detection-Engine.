7b690d067fbb5c4fc70d7507da686e3f9d8a79fd
Srikanth123-dot <golaganisrikanth123@gmail.com>
2026-09-07 17:24:02 +0000

    chore(ebpf): ignore Rust build artifacts

--- ebpf/collector/target/debug/collector.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/API-SEN/API-Sentinel-TEAM/ebpf/collector/target/debug/collector: /mnt/c/API-SEN/API-Sentinel-TEAM/ebpf/collector/src/main.rs
--- ebpf/collector/target/debug/deps/collector-5b37487662d1a8ce.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/API-SEN/API-Sentinel-TEAM/ebpf/collector/target/debug/deps/collector-5b37487662d1a8ce.d: src/main.rs
-
-/mnt/c/API-SEN/API-Sentinel-TEAM/ebpf/collector/target/debug/deps/collector-5b37487662d1a8ce: src/main.rs
-
-src/main.rs:
